da85fc623f7770e163f5b405c7d21cb1f8cac00e
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-07-25 00:48:11 +0530

    Revert to CodeIgniter 3

---
+++ application/views/pages/about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php $this->load->view("components/_common_head") ?>
+</head>
+
+<body>
+  <?php $this->load->view("components/_common_nav") ?>
+  
+  <main>
+  </main>
+  <?php $this->load->view("components/_common_js") ?>
+</body>
+
+</html>

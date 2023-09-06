1ab859210fed30ac47e2809fd98b2fa42ee5493b
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-09-06 23:36:08 +0530

    Config Change

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://localhost/mulundchashree/';
-$config['base_url'] = 'https://www.mulundchashree.com/' ;
+$config['base_url'] = 'http://localhost/mulundchashree/';
+$config['base_url'] = 'https://www.beta.mulundchashree.com/' ;
 
 /*
 |--------------------------------------------------------------------------

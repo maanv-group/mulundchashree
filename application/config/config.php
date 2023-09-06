b7cf7bfc8db8782715aeea647a3da33f053c552a
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-09-06 23:37:36 +0530

    Config Change

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/mulundchashree/';
-$config['base_url'] = 'https://www.beta.mulundchashree.com/' ;
+$config['base_url'] = 'https://beta.mulundchashree.com/' ;
 
 /*
 |--------------------------------------------------------------------------
